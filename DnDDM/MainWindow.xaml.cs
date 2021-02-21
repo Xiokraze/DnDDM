898738d3f398f7d5b1b3551e7d390da02b150ca2
Xiokraze <xiokraze@gmail.com>
2021-02-21 17:11:27 -0600

    MainWindow: Started constructing main menu and click handlers.

--- DnDDM/MainWindow.xaml.cs
+++ DnDDM/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using DnDDM.Classes;
 using DnDDM.Controls;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace DnDDM
 {
@@ -21,6 +23,55 @@ namespace DnDDM
             DefaultValues.mainWindow = this;
             this.mainMenu.Visibility = Visibility.Collapsed;
             this.contentControl.Content = new SplashControl();
+        }
+
+        private void ShowArmorControl(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowArtControl(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowGemsControl(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowGoodsControl(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowWeaponsControl(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowTrinektsControl(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MouseEnterMenuItem(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var item = sender as MenuItem;
+            item.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#930C10"));
+            item.IsSubmenuOpen = true;
+        }
+
+        private void MouseLeaveMenuItem(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var item = sender as MenuItem;
+            item.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#F7CE65"));
+            item.IsSubmenuOpen = false;
+        }
+
+        private void MenuItem_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
         }
     }
 }
